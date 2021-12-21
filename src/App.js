904d41f13eafd7b8423b1c7b54a9cdc9fd69fbc6
tezosmiami <tezosmiami@gmail.com>
2021-12-21 15:09:43 -0500

    clean up main

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
-import React, {useState, useEffect, useContext} from "react";
-import Unity, { UnityContext } from "react-unity-webgl";
-import { useUserContext } from "./UserContext" 
-import "./styles.css"
-
-const unityContext = new UnityContext({
-  loaderUrl: "build/tezRocks.loader.js",
-  dataUrl: "build/tezRocks.data",
-  frameworkUrl: "build/tezRocks.framework.js",
-  codeUrl: "build/tezRocks.wasm",
-});
+import React, { useState, useEffect, useContext } from "react";
+import { useUserContext } from "./UserContext";
+import "./styles.css";
 
 
 function App() {
 
-    const  app = useUserContext();
+  const  app = useUserContext();
+  
   return(
     <div>
-      <div>
       <header>
+      
       {app.activeAccount && app.address.substr(0, 5) + ". . ." + app.address.substr(-5)}
-    <button onClick={() => !app.activeAccount ? app.logIn() : app.logOut()}> 
+      
+      <button onClick={() => !app.activeAccount ? app.logIn() : app.logOut()}> 
+      
+      {!app.activeAccount ? "sync" : "unsync"}</button></header>
        
-        {!app.activeAccount ? "sync" : "unsync"}</button></header>
-        </div>
-<body>   
-    <Unity unityContext={unityContext}   style={{
-        height: "auto",
-        width: "90vw",
-        aspectRatio: "4 / 3",
+      <body>   
     
-      }} />
-</body>
+      </body>
+
     </div>)
 }
 
--- src/UserContext.js
+++ src/UserContext.js
@@ -6,14 +6,10 @@ import { BeaconWallet } from "@taquito/beacon-wallet";
 
 const UserContext = React.createContext();
 const options = {
-  name: 'LoginTezos'
+  name: 'Tezos Miami'
  };
   
 const wallet = new BeaconWallet(options);
-// const getActiveAccount = async() => {
-//   if (wallet.client) return await wallet.client.getActiveAccount();
-
-// };
 
 export const useUserContext = () => {
 
@@ -31,73 +27,52 @@ export const UserContextProvider = ({ children }) => {
   const [app, setApp] = React.useState("");
   const [address, setAddress] = useState("");
   const [tezos, setTezos] = useState(new TezosToolkit("https://mainnet.api.tez.ie"));
- 
-    
-const [activeAccount, setActiveAccount] = useState("")
-  
-   // var activeAccount = useActiveAccount(wallet, magic);
-  
-  // Wrap the Realm.App object's user state with React state
-  
-  
-    
+  const [activeAccount, setActiveAccount] = useState("")
 
 
-    useEffect( () => {
-      const getLoggedIn = async () => {
-
-  
-       
-         if (await wallet?.client?.getActiveAccount()) { //Tezos.setWalletProvider(wallet);
-          
-           setActiveAccount(await wallet?.client?.getActiveAccount());
-           const address =  await wallet.getPKH();
-            setAddress(address);
-            tezos.setWalletProvider(wallet);
-    setTezos(tezos)
-          }
+  useEffect( () => {
+     const getLoggedIn = async () => {
+        if (await wallet?.client?.getActiveAccount()) { 
+          setActiveAccount(await wallet?.client?.getActiveAccount());
+          const address =  await wallet.getPKH();
+          setAddress(address);
+          tezos.setWalletProvider(wallet);
+          setTezos(tezos)
+        }
       };
       getLoggedIn();
-      }, []);
+    }, []);
   
   async function logIn() {
     app.currentUser && await app.currentUser?.logOut();
     await wallet.client.clearActiveAccount();
-
-  await wallet.client.requestPermissions({
+    await wallet.client.requestPermissions({
       network: {
         type: 'mainnet',
       },
     });
-
     tezos.setWalletProvider(wallet);
     setTezos(tezos)
-  
-   setAddress(await wallet.getPKH());
-  setActiveAccount(await wallet?.client?.getActiveAccount());
+    setAddress(await wallet.getPKH());
+    setActiveAccount(await wallet?.client?.getActiveAccount());
 
   }
-    
-
 
   async function logOut() {
-    // Log out the currently active user
-   
     await wallet.client.clearActiveAccount();
-    // If another user was logged in too, they're now the current user.
-    // Otherwise, app.currentUser is null.
-    
     setActiveAccount("")
     setAddress("");
-    //window.location.reload();
+    //  window.location.reload();
   }
 
   const wrapped = { ...app, tezos, logIn, logOut, activeAccount, address};
 
   return (
+   
     <UserContext.Provider value={wrapped}>
-      {children}
+           {children}
     </UserContext.Provider>
+  
   );
 };
 
