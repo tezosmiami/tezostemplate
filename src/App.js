19c64ec3943d7247b17b4161ec63b4a0961e1b54
tezosmiami <tezosmiami@gmail.com>
2023-01-21 13:35:12 -0500

    mobile styling. . .

--- src/App.js
+++ src/App.js
@@ -4,14 +4,35 @@ import { Mint } from './components/mint'
 import Sketch from "./components/sketch";
 import "./styles.css";
 
+export const useWindowWidth = () => {
+  const [width, setWidth] = useState(0)
+  
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth)
+    }
+    window.addEventListener("resize", handleResize)
+    handleResize()
+    return () => { 
+      window.removeEventListener("resize", handleResize)
+    }
+  }, [setWidth])
+  
+  return width
+}
 
 function App() {
   const [image, setImage] = useState(null)
   const [isMint, setIsMint] = useState(false)
   const app = useTezosContext()
+  let width = useWindowWidth()
+  
   return(
     <>
-      <header>
+      <header style={{ height: `${useWindowWidth() < 600 ? '174px' : '144px'}`, 
+                       alignItems: `${useWindowWidth() < 600 ? 'start' : 'center'}`,
+                       marginTop: `${useWindowWidth() < 600 ? '11px' : '0px'}`   
+                    }}>
         {app.address && <a href={`https://hicetnunc.miami/tz/${app.address}`}
         target="blank" rel="noopener noreferrer"> 
           {app.alias || app.address.substr(0, 5) + "..." + app.address.substr(-5)}
@@ -21,7 +42,7 @@ function App() {
           {!app.acc ? "sync" : "unsync"}
         </button>
       </header>  
-      {isMint ? <Mint image={image} isMint= {isMint} setIsMint={setIsMint} /> :  <Sketch  image={image} setImage={setImage} setIsMint={setIsMint} />}
+      {isMint ? <Mint image={image} isMint= {isMint} setIsMint={setIsMint} /> :  <Sketch  image={image} setImage={setImage} setIsMint={setIsMint} width={width} />}
   
     </>
     )
--- src/components/sketch.js
+++ src/components/sketch.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import p5 from "p5";
+import { useWindowWidth } from "../App";
 import { useTezosContext } from "../context/tezos-context";
 
 let tools;
@@ -19,13 +20,14 @@ let currentBrush;
 let img;
 let select;
 let version = 1;
+let offset = 0
 
-
-export const Sketch =  ({image, setImage, setIsMint}) => {
+export const Sketch =  ({image, setImage, setIsMint, width}) => {
     const {address} = useTezosContext()
     const myRef = useRef()
     const myP5 = useRef()
 
+    
     useEffect(() => {
         myP5.current = new p5(sketch, myRef.current)
         return () => {
@@ -34,20 +36,25 @@ export const Sketch =  ({image, setImage, setIsMint}) => {
         }
 
     }, [])
-    
+
     useEffect(() => { 
         console.log('what')
          myP5.current && myP5.current.updateSync(address)
     }, [address])
     
-    
+    useEffect(() => {   
+            myP5.current.removeElements()
+            myP5.current.toolBox()
+      }, [width])
+      
 
 const sketch = (p) => {
     p.a=address
     version = 1
     p.toolBox = () => {
+        offset = p.windowWidth > 600 ? 0 : 40
         tools = p.createDiv(); 
-        tools.position(10, 10);
+        tools.position(10, 10+offset);
         tools.style("width", "370px");
         tools.style("height", "129px");
         tools.style("background-color", "white");
@@ -56,74 +63,74 @@ const sketch = (p) => {
         currentBrush = brush1
         brushOpacity = p.createSlider(0, 255, 255);
         brushSize = p.createSlider(1, 100, 50);
-        brushSize.position(20, 110);
-        brushOpacity.position(20, 80);
+        brushSize.position(20, 110+offset);
+        brushOpacity.position(20, 80+offset);
 
         let button1 = p.createButton("");
         button1.mousePressed(() => currentBrush = brush1);
-        button1.position(20, 20);
+        button1.position(20, 20+offset);
         button1.size(50, 50);
         button1.style("background-image", `url(images/image${version === 1 ? '1' : '7'}.png)`);
         button1.style("background-size", "cover");
         
         let button2 = p.createButton("");
         button2.mousePressed(() => currentBrush = brush2);
-        button2.position(80, 20);
+        button2.position(80, 20+offset);
         button2.size(50, 50);
         button2.style("background-image", `url(images/image${version === 1 ? '2' : '8'}.png)`);
         button2.style("background-size", "cover");
         
         let button3 = p.createButton("");
         button3.mousePressed(() => currentBrush = brush3);
-        button3.position(140, 20);
+        button3.position(140, 20+offset);
         button3.size(50, 50);
         button3.style("background-image", `url(images/image${version === 1 ? '3' : '9'}.png)`);
         button3.style("background-size", "cover");
         
         let button4 = p.createButton("");
         button4.mousePressed(() => currentBrush = brush4);
-        button4.position(200, 20);
+        button4.position(200, 20+offset);
         button4.size(50, 50);
         button4.style("background-image", `url(images/image${version === 1 ? '4' : '10'}.png)`);
         button4.style("background-size", "cover");
         
         let button5 = p.createButton("");
         button5.mousePressed(() => currentBrush = brush5);
-        button5.position(260, 20);
+        button5.position(260, 20+offset);
         button5.size(50, 50);
         button5.style("background-image", `url(images/image${version === 1 ? '5' : '11'}.png)`);
         button5.style("background-size", "cover");
         
         let button6 = p.createButton("");
         button6.mousePressed(() => currentBrush = brush6);
-        button6.position(320, 20);
+        button6.position(320, 20+offset);
         button6.size(50, 50);
         button6.style("background-image", `url(images/image${version === 1 ? '6' : '12'}.png)`);
         button6.style("background-size", "cover");
 
         let button7 = p.createButton("");
         button7.mousePressed(() => currentBrush = brush7);
-        button7.position(170, 80);
+        button7.position(170, 80+offset);
         button7.size(50, 50);
         button7.style("background-image", `url(images/black.png)`);
         button7.style("background-size", "cover");
         
         let button8 = p.createButton("");
         button8.mousePressed(() => currentBrush = brush8);
-        button8.position(230, 80);
+        button8.position(230, 80+offset);
         button8.size(50, 50);
         button8.style("background-image", `url(images/tan.png`);
         button8.style("background-size", "cover");
         
         select = p.createSelect();
-        select.position(310, 80);
+        select.position(310, 80+offset);
         select.option('v.1');
         select.option('v.2');
         select.value(version === 1 ? 'v.1' : 'v.2')
         select.changed(p.selectVersion);
 
         saveButton = p.createButton(p.a ? "Mint" : "Save");
-        saveButton.position(305, 105);
+        saveButton.position(305, 105+offset);
         saveButton.mousePressed(() => {
             if (p.a) {    
                 setImage(myP5.current.canvas.toDataURL())
